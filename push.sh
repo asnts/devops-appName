4206ef6c512cfb89062f867b63f3e42890a7d6da
annanda-santos <annanda.santos@ibm.com>
2023-04-13 10:23:43 -0300

    create script push.sh

---
+++ push.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#######################################################################################
+# Autora: Annanda Santos
+# Email da autora: annandasants@gmail.com
+# Descrição: Automatização de push de imagens para docker hub
+# Repositório Remoto:
+# Parâmetros: N/A
+# Exemplo de uso:
+# ./push.sh
+#######################################################################################
+
+if [ -z ${DOCKER_USERNAME} ]; then
+    echo "Missing DOCKER_USERNAME variable!"
+    exit 1
+fi
+
+error() {
+    if [ $? != 0 ]; then
+        echo "Error!"
+        exit 122
+    fi
+}
+
+build() {
+    echo "=> Building ${1}"
+    docker build -t ${1} .
+    echo "=> Built ${1}"
+}
+
+tag() {
+    echo "=> Tagging ${1}"
+    docker tag ${1} $(echo $DOCKER_USERNAME)/${1}
+    echo "=> Tagged ${1}"
+}
+
+push() {
+    echo "=> Pushing ${1}"
+    docker push $(echo $DOCKER_USERNAME)/${1}
+    echo "=> Pushed ${1}"
+}
+
+build ${1}
+error
+tag ${1}
+error
+push ${1}
+error
+echo
+
+exit 0
